5478466d3a50bcb0696851e44a4ba6308de150f4
Ankam592 <manojkumarankam0405@gmail.com>
2025-08-03 08:52:51 +0530

    node modules

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import {Footer} from './components/Index';
-import {Header} from './components/Header/header'  
+import {Header} from './components/header/Header'  
 import { login_user, logout } from './store/authSlice';
 import './App.css'
 
